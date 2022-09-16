87277ebb60371d293127ff6f5d4328b359151907
paul-nguyen96 <paul121596@icloud.com>
2022-09-15 22:52:55 -0500

    initial commit // layout for navbar and aboutme

---
+++ src/components/layout/Hero.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div className="hero min-h-screen bg-base-200">
+    <div className="hero-content text-start">
+      <div className="max-w-5xl">
+
+        <h1 className="text-5xl font-bold mb-4">Paul Nguyen</h1>
+        <h1 className="text-5xl font-bold">I like to build things for the web.</h1>
+        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+        <button className="btn btn-primary">
+        <a href="https://github.com/paul-nguyen96">Check out my Github!</a></button>
+      </div>
+    </div>
+  </div>
+  )
+}
+
+export default Hero
---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import { FaHome } from "react-icons/fa";
+
+function Navbar(){
+
+
+
+  return (
+    <div className="navbar bg-base-100 sticky top-0 z-50">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 font-bold"
+          >
+            <li>
+              <a href="#AboutMe">About Me</a>
+            </li>
+            <li>
+              <a href="#Projects">Projects</a>
+            </li>
+            <li>
+              <a href="#Contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+        <a href="/" className="btn btn-ghost normal-case text-3xl">
+          <FaHome />
+        </a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal p-0 font-bold">
+          <li>
+            <a href="#AboutMe">About Me</a>
+          </li>
+          <li>
+            <a href="#Projects">Projects</a>
+          </li>
+          <li>
+            <a href="#Contact">Contactt</a>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-end">
+        <a href="https://docs.google.com/document/d/1GWFFO71J9pzJpT4z8BiqwRqrRfwThPovtUMfAoZNufM/" className="btn">Resume</a>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
